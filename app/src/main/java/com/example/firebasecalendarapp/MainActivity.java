e11bd08bd7b42875a3107ed957485ccbebb6be26
ara <ameliaarahayu138@gmail.com>
2023-12-19 17:34:10 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/firebasecalendarapp/MainActivity.java
+++ app/src/main/java/com/example/firebasecalendarapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -15,6 +16,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -71,12 +74,14 @@ public class MainActivity extends AppCompatActivity {
         eventAdapter = new EventAdapter(eventList, new EventAdapter.EventClickListener() {
             @Override
             public void onEditClick(int position) {
-                // Handle klik tombol edit pada item RecyclerView
-                EventModel event = eventList.get(position);
-                editText.setText(event.getEventName());
-                lastEditedPosition = position;
+                if (position >= 0 && position < eventList.size()) {
+                    EventModel event = eventList.get(position);
+                    editText.setText(event.getEventName());
+                    lastEditedPosition = position;
+                } else {
+                    // Handle an invalid position, log an error, or show a message.
+                }
             }
-
             @Override
             public void onDeleteClick(int position) {
                 // Handle klik tombol delete pada item RecyclerView dengan konfirmasi
@@ -88,9 +93,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(eventAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
-
-
-
 
     private void showDeleteConfirmationDialog(int position) {
         // Membuat AlertDialog untuk konfirmasi penghapusan
@@ -153,21 +155,111 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Menyimpan atau memperbarui event ke Firebase
     public void buttonSaveEvent(View view) {
         String eventName = editText.getText().toString();
-        if (!eventName.isEmpty()) {
+        if (!eventName.isEmpty() && stringDateSelected != null) {
             if (lastEditedPosition != -1) {
-                // Jika sedang mengedit, update event yang sudah ada
-                updateEvent(lastEditedPosition, eventName);
-                lastEditedPosition = -1;
+                // Jika sedang mengedit, munculkan konfirmasi sebelum mengupdate event
+                if (isDateSelectedCorrect()) {
+                    updateEventWithConfirmation(lastEditedPosition, eventName);
+                    lastEditedPosition = -1;
+                } else {
+                    // Tampilkan pesan kesalahan jika tanggal yang dipilih tidak sesuai
+                    showDateMismatchError();
+                }
             } else {
                 // Jika tidak sedang mengedit, tambahkan event baru
                 String eventKey = databaseReference.child("Event").child(stringDateSelected).push().getKey();
                 databaseReference.child("Event").child(stringDateSelected).child(eventKey).setValue(eventName);
+                updateEventList();
+                clearEditText();
             }
-            updateEventList();
-            clearEditText();
+        } else {
+            // Tambahkan penanganan jika stringDateSelected null
+            Log.e("SaveEvent", "Invalid stringDateSelected");
+            Toast.makeText(this, "Pilih tanggal terlebih dahulu", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Metode untuk menampilkan pesan kesalahan jika tanggal yang dipilih tidak sesuai
+    private void showDateMismatchError() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Kesalahan");
+        builder.setMessage("Maaf, tidak bisa mengupdate event karena tanggal yang dipilih tidak tepat. Mohon periksa kembali.");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    // Metode untuk mengecek apakah tanggal yang dipilih sesuai dengan tanggal yang sedang diedit
+    private boolean isDateSelectedCorrect() {
+        // Implementasi metode
+        return true;
+    }
+
+    // Memperbarui nama event di Firebase dengan konfirmasi
+    private void updateEventWithConfirmation(int position, String newName) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Konfirmasi Update");
+        builder.setMessage("Apakah Anda yakin ingin mengupdate event?");
+
+        // Tombol OK (Update)
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                updateEvent(position, newName);
+            }
+        });
+
+        // Tombol Batal
+        builder.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss(); // Tutup dialog tanpa mengupdate
+            }
+        });
+
+        // Menampilkan AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    // Metode untuk memperbarui event di Firebase
+    private void updateEvent(int position, String newName) {
+        if (position >= 0 && position < eventList.size()) {
+            EventModel event = eventList.get(position);
+            if (event != null) {
+                event.setEventName(newName);
+                String eventKey = event.getEventKey();
+                databaseReference.child("Event").child(stringDateSelected).child(eventKey).setValue(newName)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    // Update berhasil, refresh daftar event
+                                    updateEventList();
+                                    // Clear EditText setelah berhasil update
+                                    clearEditText();
+                                } else {
+                                    // Gagal melakukan update
+                                    Log.e("UpdateEvent", "Failed to update event", task.getException());
+                                    Toast.makeText(MainActivity.this, "Gagal mengupdate event", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                eventAdapter.notifyItemChanged(position);
+            } else {
+                // Log atau tangani kondisi ketika objek event bernilai null
+                Log.e("UpdateEvent", "Event object is null at position: " + position);
+            }
+        } else {
+            // Log atau tangani kondisi ketika posisi tidak valid
+            Log.e("UpdateEvent", "Invalid position: " + position);
         }
     }
 
@@ -183,15 +275,6 @@ public class MainActivity extends AppCompatActivity {
             // Log jika posisi yang dihapus tidak valid
             Log.e("DeleteEvent", "Invalid position: " + position);
         }
-    }
-
-    // Memperbarui nama event di Firebase
-    private void updateEvent(int position, String newName) {
-        EventModel event = eventList.get(position);
-        event.setEventName(newName);
-        String eventKey = event.getEventKey();
-        databaseReference.child("Event").child(stringDateSelected).child(eventKey).setValue(newName);
-        eventAdapter.notifyItemChanged(position);
     }
 
     // Membersihkan EditText setelah menyimpan event
